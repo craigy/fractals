79c7b38130b06296746650c0ffc512e5d58d9a4c
craigy <craigy@users.noreply.github.com>
2016-03-16 22:02:41 -0400

    Start playing around with html canvas.
    Add css.
    Add link to project page.

--- src/cljs/fractals/core.cljs
+++ src/cljs/fractals/core.cljs
@@ -9,9 +9,36 @@
     {:roster {}
      :result nil}))
 
+(defn canvas-component[]
+  [:canvas
+   {:id "canvas"
+    :width "380"
+    :height "380"}])
+
 (defn app []
   [:div
-   [:h1 "Fractals"]])
+   [:a {:href "https://github.com/craigy/fractals"}
+     [:h1 "Fractals"]]
+   [canvas-component]])
+
+(when (:interval @app-state)
+  (js/clearInterval (:interval @app-state)))
+
+(defn rand-color []
+  (str "#" (.toString (int (rand 16777215)) 16)))
+
+(let [interval
+  (js/setInterval
+    #(let [canvas (js/document.getElementById "canvas")]
+      (when canvas
+        (let [ctx (.getContext canvas "2d")
+              width (.-width canvas)
+              height (.-height canvas)
+              img (.createImageData ctx width height)
+              pixels (.-data img)]
+          (set! (.-fillStyle ctx) (rand-color))
+          (.fillRect ctx 0 0 width height)))) 1000)]
+  (swap! app-state assoc :interval interval))
 
 (defn ^:export run[]
   (r/render
