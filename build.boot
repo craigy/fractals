275a08257c7d71f02779f1705c81e1525cca1077
craigy <craigy@users.noreply.github.com>
2016-03-20 13:43:25 -0400

    Fix color rendering.
    Add favicon.
    Remove reagent dependency.

--- build.boot
+++ build.boot
@@ -11,8 +11,7 @@
                  [com.cemerick/piggieback   "0.2.1"     :scope "test"]
                  [weasel                    "0.7.0"     :scope "test"]
                  [org.clojure/tools.nrepl   "0.2.12"    :scope "test"]
-                 [boot/core                 "2.5.5"]
-                 [reagent                   "0.6.0-alpha"]])
+                 [boot/core                 "2.5.5"]])
 
 (require '[adzerk.boot-cljs :refer [cljs]]
          '[pandeiro.boot-http :refer [serve]]
--- src/cljs/fractals/core.cljs
+++ src/cljs/fractals/core.cljs
@@ -1,66 +1,26 @@
-(ns fractals.core
-  (:require
-    [reagent.core :as r]))
+(ns fractals.core)
 
 (enable-console-print!)
 
-(defonce app-state
-  (r/atom
-    {:roster {}
-     :result nil}))
-
-(defn canvas-component[]
-  [:canvas
-   {:id "canvas"
-    :width "380"
-    :height "380"}])
-
-(defn app []
-  [:div
-   [:a {:href "https://github.com/craigy/fractals"}
-     [:h1 "Fractals"]]
-   [canvas-component]])
-
-(when (:interval @app-state)
-  (js/clearInterval (:interval @app-state)))
-
-(defn color-str [color-int]
-  (str "#" (.toString color-int 16)))
-
-(defn rand-color []
-  (color-str (int (rand 16777215))))
-
-(defn draw-random! []
-  (let [canvas (js/document.getElementById "canvas")]
-    (when canvas
-      (let [ctx (.getContext canvas "2d")
-            width (.-width canvas)
-            height (.-height canvas)
-            img (.createImageData ctx width height)
-            pixels (.-data img)]
-        (dorun
-          (map
-            (fn [[x y]]
-              (set! (.-fillStyle ctx) (rand-color))
-              (.fillRect ctx x y 1 1))
-            (for [x (range width)
-                  y (range height)]
-              [x y])))))))
+(defn color-str
+  ([r g b]
+   (str "#" (.toString r 16) (.toString g 16) (.toString b 16))))
 
 (defn mandelbrot-color [px py width height]
   (let [x0 (- (* 3.5 (/ px width)) 2.5)
-        y0 (- (* 2 (/ py height)) 1)
-        max-iter 1000]
+        y0 (- (* 3 (/ py height)) 1.5)
+        max-iter 800]
     (loop [x 0.0
            y 0.0
            iter 0]
       (if
         (and (< (+ (* x x) (* y y)) (* 2 2)) (< iter max-iter))
         (recur (+ (- (* x x) (* y y)) x0) (+ (* 2 x y) y0) (inc iter))
-        (color-str (int (* iter (/ 16777215 max-iter))))))))
+        (let [d (int (* iter (/ 255 max-iter)))]
+          (color-str d d d))))))
 
 (defn draw-mandelbrot! []
-  (let [canvas (js/document.getElementById "canvas")]
+  (let [canvas (js/document.getElementById "mandelbrot")]
     (when canvas
       (let [ctx (.getContext canvas "2d")
             width (.-width canvas)
@@ -76,20 +36,5 @@
                   y (range height)]
               [x y])))))))
 
-(js/setTimeout
-  draw-mandelbrot!
-  1000)
-
-;(let [interval
-;  (js/setInterval
-;    draw-random!
-;    1000)]
-;  (swap! app-state assoc :interval interval))
-
-(defn ^:export run[]
-  (r/render
-    [app]
-    (js/document.getElementById "app")))
-
-(run)
+(draw-mandelbrot!)
 
